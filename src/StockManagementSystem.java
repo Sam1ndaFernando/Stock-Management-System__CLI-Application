dc424edb3ddbe49e4c3c40e9bae680f85aa1983c
Sam1ndaFernando <uthpala2001@example.com>
2023-06-26 08:35:46 +0530

    Add Supplier, Update Supplier, Delete Supplier

--- src/StockManagementSystem.java
+++ src/StockManagementSystem.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StockManagementSystem {
@@ -8,11 +9,16 @@ public class StockManagementSystem {
     public static void main(String[] args) {
         login[0][0] = "saminda";
         login[0][1] = "1234";
+
         login();
-        ChangeTheCredentials();
-        SupplierManage();
 
+        clearConsole();
+        HomePage();
 
+//        ChangeTheCredentials();
+//        SupplierManage();
+        //AddSupplier();
+        //updateSupplier();
     }
 
     private static void login() {
@@ -39,7 +45,6 @@ public class StockManagementSystem {
                     clearConsole();
                     HomePage();
                     //System.out.println("PIT thama !");
-
 
                 } else {
                     System.out.println("password is incorrect. please try again!");
@@ -105,18 +110,246 @@ public class StockManagementSystem {
     }
 
     private static void SupplierManage() {
+        int option;
+        while(true){
+            System.out.println("+---------------------------------------------------------------------------------------+");
+            System.out.println("|\t\t\t\t\t\t\t\tSUPPLIER MANAGE\t\t\t\t\t\t\t\t\t\t\t|");
+            System.out.println("+---------------------------------------------------------------------------------------+");
+            System.out.println();
+            System.out.println("[1] Add Supplier          "+"                  "+"[2] Update Supplier");
+            System.out.println("[3] Delete Supplier       "+"                  "+"[4] View Supplier  ");
+            System.out.println("[5] Search Supplier       "+"                  "+"[6] Home Page      ");
+
+            System.out.println();
+
+            System.out.print("Enter an option to continue > ");
+            option = input.nextInt();
+
+            switch (option){
+                case 1 :
+                    clearConsole();
+                    AddSupplier();
+                    break;
+
+                case 2 :
+                    clearConsole();
+                    updateSupplier();
+                    break;
+
+                case 3 :
+                    clearConsole();
+                    DeleteSupplier();
+                    break;
+            }
+        }
+    }
+    private static void DeleteSupplier() {        ///explain
         System.out.println("+---------------------------------------------------------------------------------------+");
-        System.out.println("|\t\t\t\t\t\t\t\t\tSUPPLIER MANAGE\t\t\t\t\t\t\t\t\t\t|");
+        System.out.println("|\t\t\t\t\t\t\t\tDELETE SUPPLIER\t\t\t\t\t\t\t\t\t\t\t|");
         System.out.println("+---------------------------------------------------------------------------------------+");
         System.out.println();
+        while (true){
+            System.out.print("Supplier id : ");
+            String id = input.next();
+
+            boolean isValidSupplierID = checkSupplierId(id);
+            if (isValidSupplierID) {
+                for (int i = 0; i < supplies.length; i++) {
+                    if (supplies[i][0].equals(id)) {
+                        String[][] updateSuppliers = new String[supplies.length-1][2];
+
+                        int count=0;
+                        for (int j = 0; j < supplies.length; j++) {
+                            if (j!=i) {
+                                updateSuppliers[count]=supplies[j];
+                                count++;
+                            }
+                        }
+                        supplies = updateSuppliers;
+                        System.out.println("deleted successfully! ");
+                        System.out.print("Do you want to delete another(Y/N) ? ");
+                        String answer = input.next();
+
+                        if (answer.equalsIgnoreCase("Y") || answer.equalsIgnoreCase("y")) {
+                            clearConsole();
+                            DeleteSupplier();
+                            break;
+                        }
+                        if (answer.equalsIgnoreCase("N") || answer.equalsIgnoreCase("n")) {
+                            clearConsole();
+                            SupplierManage();
+                            break;
+                        }
+                        //return;
+                    }
+                }
+            }else{
+                System.out.println("can't find supplier id. try again!");
+            }
+        }
 
     }
 
+    private static boolean checkSupplierId(String id) {
+        //check Supplier id and return it
+
+        for (int i = 0; i < supplies.length; i++) {
+            if (id.equals(supplies[i][0])) {           //explain
+                return true;
+            }
+        }
+        return false;
+    }
+    private static int getIndexString(String id) {
+        for (int i = 0; i < supplies.length; i++) {
+            if (id.equals(supplies[i][0])) {
+                return i;
+            }
+        }
+        return 0;
+    }
+    private static void updateSupplier() {
+        boolean duplicate;
+
+        System.out.println("+---------------------------------------------------------------------------------------+");
+        System.out.println("|\t\t\t\t\t\t\t\tUPDATE SUPPLIER\t\t\t\t\t\t\t\t\t\t\t|");
+        System.out.println("+---------------------------------------------------------------------------------------+");
+        System.out.println();
+        while (true){
+            System.out.print("Supplier ID : ");
+            String id = input.next();
+            duplicate=checkDuplicate(id);
+
+            String answer;
+            String readStudentName= readStudentName(id);
+            if (duplicate==false) {
+                System.out.println("can't find supplier id. try again!");
+            } else if (duplicate==true) {
+                System.out.print("Supplier Name : ");
+                String newName = input.next();
+
+                // update name on student array
+                for (int i = 0; i < supplies.length; i++) {
+                    if (id.equals(supplies[i][0])) {
+                        supplies[i][1]=newName;
+                    }
+                }
+                System.out.println(Arrays.deepToString(supplies));
+                System.out.print("added successfully! ");
+                System.out.print("Do you want to update another suplier(Y/N) ? ");
+                answer = input.next();
+
+                if (answer.equalsIgnoreCase("Y") || answer.equalsIgnoreCase("y")) {
+                    clearConsole();
+                    updateSupplier();;
+                    break;
+                }
+                if (answer.equalsIgnoreCase("N") || answer.equalsIgnoreCase("n")) {
+                    clearConsole();
+                    SupplierManage();
+                }
+            }
+        }
+    }
+    private static String readStudentName(String id) {
+        for (int i = 0; i < supplies.length; i++) {
+            for (int j = 0; j < supplies[i].length; j++) {
+                if (id.equals(supplies[i][0])) {
+                    return supplies[i][1];      // check suppliyer id and return his name
+                }
+            }
+        }
+        return "";
+    }
+
+    private static void AddSupplier() {
+        boolean duplicate;
+
+            System.out.println("+---------------------------------------------------------------------------------------+");
+            System.out.println("|\t\t\t\t\t\t\t\tADD SUPPLIER\t\t\t\t\t\t\t\t\t\t\t|");
+            System.out.println("+---------------------------------------------------------------------------------------+");
+            System.out.println();
+        while (true){
+            System.out.print("Supplier id : ");
+            String id = input.next();
+
+            duplicate = checkDuplicate(id);
+
+            if (duplicate==true) {
+                System.out.println("alrady exists. try another supplier id!");
+                //break;
+            } else if (duplicate==false) {
+                System.out.print("Supplier Name : ");
+                String name = input.next();
+
+                extendAddSupplier(id,name);
+               //extendMarksStudent();
+                String answr ;
+
+                do{
+                    System.out.println();
+                    System.out.print("added successfully! ");
+                    System.out.print("Do you want to add another supplier(Y/N) ?");    ///
+                    answr=input.next();
+
+                    if (answr.equalsIgnoreCase("Y") || answr.equalsIgnoreCase("y")){
+                        clearConsole();
+                        AddSupplier();
+                        break;
+                    }
+
+                    if (answr.equalsIgnoreCase("N") || answr.equalsIgnoreCase("n")) {
+                        System.out.println();
+                        clearConsole();
+                        SupplierManage();
+                        break;
+                    }
+                    //System.out.println("code loku wadii hu@#! ");
+                    return;
+
+            }while (true);
+            }
+        }
+    }
+
+//    private static int getUserAnswer() {
+//        char answr = input.next().charAt(0);
+//
+//        if (answr == 'Y' || answr == 'y') {
+//            return  1;
+//        } else if (answr=='N' || answr=='n') {
+//            return 2;
+//        }else {
+//            return 0;
+//        }
+//    }
+
+    private static void extendAddSupplier(String id, String name) {              //Assign temp array values
+        String[][] temp = new String[supplies.length+1][2];
+
+        for (int i = 0; i < supplies.length; i++) {
+            for (int j = 0; j < supplies[i].length; j++) {
+                temp[i][j]= supplies[i][j];
+            }
+        }
+        supplies = temp;
+
+        supplies[supplies.length-1][0] = id;   ///// ?????????????????????????   explain
+        supplies[supplies.length-1][1]=name;
+        System.out.println(Arrays.deepToString(supplies));
+    }
+    private static boolean checkDuplicate(String id) {
+        for (int i = 0; i < supplies.length; i++) {
+            if (supplies[i][0].equals(id)) {
+                return true;
+            }
+        }
+        return false;
+    }
     private static void logout(int i) {
         if (i==4) {
             login();
         }
-
     }
 
     private static void ChangeTheCredentials() {
@@ -127,7 +360,6 @@ public class StockManagementSystem {
         System.out.println("|\t\t\t\t\t\t\t\tCREDENTIAL MANAGE\t\t\t\t\t\t\t\t\t\t|");
         System.out.println("+---------------------------------------------------------------------------------------+");
         System.out.println();
-
 
         do {
             System.out.print("Please enter the user name to verify it's you : ");
@@ -153,7 +385,6 @@ public class StockManagementSystem {
 
                         System.out.print("Password changed successfully! Do you go to home page (Y/N): ");                        /// continue the progtrammme
                         String goToHomePage = input.next();
-
 
                         if (goToHomePage.equalsIgnoreCase("Y") || goToHomePage.equalsIgnoreCase("y")){
                             clearConsole();
